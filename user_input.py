f2a1ea09360cf8c4b05eb1ae13c8a17d11395a45
dantekadagi3 <dantekadagi3@gmail.com>
2024-08-29 15:00:51 +0300

    assignment1 plp on python

---
+++ user_input.py
@@ -0,0 +1,7 @@
+
+
+name = input("Enter your Name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print("Hello", name, "you are now", age, "years old and live in", location)
